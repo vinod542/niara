319c81d8054cc93051a103521ffcd7c601342620
vinod542 <vinodv@hpe.com>
2019-03-19 15:13:14 -0700

    Create workflowE.py

---
+++ workflowE.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+import os, sys
+import subprocess
+from fnmatch import fnmatch
+
+
+def batchlog(s1, filename, workflow):
+     route = path+'/'+filename
+     with open(route , 'r|') as logfile, open(s1 + ".txt", 'w|') as file1::
+          for logline in logfile:
+               if any(part in line for part in "Start workflow "+s1):
+                    begin="Start workflow "+workflow
+                    end="Finish workflow "+s1
+                    file1.write(subprocess.call([‘sed’, ‘s/begin/end/g’,route]))
+                    
+def flocation(pattern, s1, workflow):
+     path='/var/log/analyzer_sched/'
+     for path, subdirs, files in os.walk(path):
+          for filename in files:
+               if fnmatch(filename, pattern):
+                    batchlog (s1, filename, workflow)
+                    
+if __name__ == '__main__':
+    workflows= ['BatchDataBackupPipeLine', 'CefFeyeCorrPipeLine', 'EntityScoring', 'EventAggregator', 'FeedPipeLine', 'FilePurger', 'LogIngestionPipeLine' 'ObjectPipeLine', 'RetentionPurgerPipeLine',
+                'ThreatCentralUploader', 'UserTimeline', 'WatchlistPurger', 'GenericUBA', 'EflowCorrelation', 'CorrelationBulkLoader', 'EntityAuthProfiler', 'RuleEnginePipeLine', 'EventGenerator',
+                'CacheTransform']
+    with open (sys.argv[1], 'r') as inputf:
+         for line in inputf:
+              if any(word in line for word in workflows):
+                   s1 = line.split(' ')[0]
+                   workflow = line
+                   if s1 == 'EflowCorrelation':
+                        pattern = 'eflow_corr.l*'
+                        flocation(pattern):
+                   elif s1 == 'GenericUBA':
+                        pattern = 'generic_uba.l*'
+                        flocation(pattern):
+                   elif s1 == 'CacheTransform':
+                        pattern = 'cache_transform.l*'
+                        flocation(pattern):
+                   elif s1 == 'CefFeyeCorrPipeLine':
+                        pattern = 'cef_feye_corr.l*'
+                        flocation(pattern):
+                   elif s1 == 'CorrelationBulkLoader':
+                        pattern = 'corr_bulkloader.l*'
+                        flocation(pattern):
+                   elif s1 == 'EntityAuthProfiler':
+                        pattern = 'entity_auth_profiling.l*'
+                        flocation(pattern):
+                   elif s1 == 'EntityScoring':
+                        pattern = 'entity_scoring.l*'
+                        flocation(pattern):
+                   elif s1 == 'EventAggregator':
+                        pattern = 'event_aggregator.l*'
+                        flocation(pattern):
+                   elif s1 == 'ThreatCentralUploader':
+                        pattern = 'threat_central_uploader.l*'
+                        flocation(pattern):
+                   elif s1 == 'UserTimeline':
+                        pattern = 'user_timeline.l*'
+                        flocation(pattern):
+                   elif s1 == 'WatchlistPurger':
+                        pattern = 'watchlist_purger.l*'
+                        flocation(pattern):
+                   else:
+                   print "Done"
+                    
+pass
