0c12ed12991db1359b74b26e9a2c7b07154cf507
vinod542 <18684770+vinod542@users.noreply.github.com>
2019-03-20 10:34:28 -0700

    Update unass_p_shards.sh

--- unass_p_shards.sh
+++ unass_p_shards.sh
@@ -6,7 +6,7 @@ curl cdh-2:9200/_cat/nodes?v | awk '{print $NF}' | sed 1d > nodes
 while read lines
    do
         word=lines | awk '{print $1}'
-        number=lines | awk '{print $1}'
+        number=lines | awk '{print $2}'
         node=$(sort --random-sort nodes | head -n 1)
         curl -XPOST 'cdh-1:9200/_cluster/reroute?pretty' -H 'Content-Type: application/json' -d'{"commands" : [{"allocate_stale_primary" : {"index" : "$word", "shard" : $number,"node" : "$node","accept_data_loss" : true}}]}'
    done < unassignedshards.txt
