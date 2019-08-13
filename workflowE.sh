c32ce445e45e74bfa27c508edb1460c1243437cf
vinod542 <18684770+vinod542@users.noreply.github.com>
2019-08-13 10:28:07 -0700

    Update workflowE.sh

--- workflowE.sh
+++ workflowE.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 echo "create view jointly as select workflow.batch_id, workflow.name, workflow.start_time, workflow.end_time, workflow.end_time-workflow.start_time as duration, workflow.status, workflow.message, workflow.data_status, active_events.source_instance, active_events.reported_time, active_events.cleared_time, active_events.source_module, active_events.is_alarm from workflow inner join active_events on workflow.name = active_events.source_instance where workflow.message != 'success' and active_events.reported_time > current_date - interval '15 days' and source_module = 'wfe' order by start_time;" | psql -d niara
-echo "select distinct on (name) name from jointly;" | psql -d niara | sed '1,2d' | head -n -2 > failedWF.txt
-while read wflows
-        do
-        echo "select name, batch_id from jointly where name = '$wflows' order by batch_id desc limit 7;" | psql -d niara | sed '1,2d' | awk '{print $1,$3}' | head -n -2 >> failedbatches.txt;
-        done < failedWF.txt
-python workflowE.py failedbatches.txt
+echo "select distinct on (name) name from jointly;" | psql -d niara | sed '1,2d' | head -n -2 | while read lines; do echo "select name, batch_id from jointly where name = '$lines' order by batch_id desc limit 7;" | psql -d niara | sed '1,2d' | awk '{print $1,$3}' | head -n -2 >> failedbatches.txt; done
 echo "DROP view IF EXISTS jointly;" | psql -d niara
+python workflow.py failedbatches.txt
